1d4521969b2588445bea6e3de9de04e5d661866e
Muhammad Hasnain <Muhammad Hasnain>
2024-10-05 10:14:30 +0500

    Added addBook and listBooks functions to Library class

---
+++ OneDrive/Desktop/JAVA_Programmes/gitHubSession/src/gitHubSession/Library.java
@@ -0,0 +1,28 @@
+package gitHubSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+	private List<String> books=new ArrayList<>();
+	
+	public void addBook(String bookName)
+	{
+		books.add(bookName);
+		System.out.println(bookName+" Added to Library" );
+	}
+	
+	
+	public void listBooks()
+	{
+		System.out.println("\nBooks in Library"+books );
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
